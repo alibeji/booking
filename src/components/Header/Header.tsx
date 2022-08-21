2b346dbf95460661916fc72ed5e03d32c7161cbf
alibeji98 <alibeji98@gmail.com>
2022-08-21 13:42:24 +0200

    fix: clean header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,15 +3,15 @@ import { useQuery } from "react-query";
 import fetchRestaurantData from "../../utils/api/fetchRestaurantData";
 
 const restaurantId = "62c1a011e95e96a91dbfd023";
-/* type HeaderProps = {}; */
 
 export default function Header() {
   const { data, status } = useQuery("restaurants", () =>
     fetchRestaurantData(restaurantId)
   );
-  console.log(status);
-  console.log(data);
-  console.log("hi");
 
-  return <div> Reservation at {data?.name}</div>;
+  return (
+    <div>
+      Reservation at {data?.name} {status}
+    </div>
+  );
 }
