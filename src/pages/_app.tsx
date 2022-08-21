e8fab16000f1a075c992e0f8b955549ae2653c60
alibeji98 <alibeji98@gmail.com>
2022-08-21 13:35:36 +0200

    feat: add recoil and material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,30 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Layout from "../components/Layout/Layout";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const queryClient = new QueryClient();
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#EC691A",
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </QueryClientProvider>
+      </RecoilRoot>
+    </ThemeProvider>
   );
 }
 
